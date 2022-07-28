f185e63fbe1766772e59dabed70472b835c452fc
Luis <Luistejeda0011@gmail.com>
2022-07-28 15:30:56 -0300

    Working send email

--- src/services/job.service.js
+++ src/services/job.service.js
@@ -89,11 +89,7 @@ export const deleteJobs = async (Job_ID) => {
 };
 
 export const postulate = async (file) => {
-  return await axios.post(`${process.env.REACT_APP_API_URL}/jobs/postulate`, file,{ 
-    withCredentials: true,
-    headers: {
-    'Content-Type': 'multipart/form-data'
-  }})
+  return await axios.post(`${process.env.REACT_APP_API_URL}/jobs/postulate`, file,{})
   .then((response)=>{
     return response.data
   })
